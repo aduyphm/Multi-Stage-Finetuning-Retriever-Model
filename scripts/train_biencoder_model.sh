03592e6ad01f5f3149f7e90911a27c08a060699a
aduyphm <64310267+aduyphm@users.noreply.github.com>
2023-09-18 15:06:51 +0700

    15:00 2023/09/18

--- scripts/train_biencoder_model.sh
+++ scripts/train_biencoder_model.sh
@@ -7,7 +7,7 @@ DIR="$( cd "$( dirname "$0" )" && cd .. && pwd )"
 echo "working directory: ${DIR}"
 
 if [ -z "$OUTPUT_DIR" ]; then
-  OUTPUT_DIR="${DIR}/checkpoint/biencoder_$(date +%F-%H%M.%S)"
+  OUTPUT_DIR="${DIR}/checkpoints/biencoder_$(date +%F-%H%M.%S)"
 fi
 if [ -z "$DATA_DIR" ]; then
   DATA_DIR="${DIR}/data/"
@@ -17,21 +17,22 @@ mkdir -p "${OUTPUT_DIR}"
 
 PROC_PER_NODE=$(nvidia-smi --list-gpus | wc -l)
 # python -u -m torch.distributed.launch --nproc_per_node ${PROC_PER_NODE} src/train_biencoder.py \
-deepspeed src/train_biencoder.py --deepspeed ds_config.json \
+deepspeed train_biencoder.py --deepspeed ds_config.json \
     --model_name_or_path intfloat/multilingual-e5-large \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 32 \
     --add_pooler False \
+    --l2_normalize True \
     --t 0.02 \
     --seed 1234 \
     --do_train \
     --fp16 \
-    --train_file "bm25_viquad_top20.json" \
+    --train_dir "bm25" \
     --corpus_file "viquad.json"
 #     --validation_file "${DATA_DIR}/dev.jsonl" \
     --q_max_len 32 \
     --p_max_len 144 \
-    --train_n_passages 16 \
+    --train_n_passages 20 \
     --dataloader_num_workers 1 \
     --num_train_epochs 3 \
     --learning_rate 2e-5 \
